3c4f427fcf5c9d40a585ed5d7a1255c4367e9194
Felix Angelov <felangelov@gmail.com>
2021-09-13 13:31:26 -0500

    docs: fix README formatting and add coverage badge (#11)

--- example/main.dart
+++ example/main.dart
@@ -6,10 +6,12 @@ Future<void> main() async {
 
   // Check whether a package is up to date.
   final isUpToDate = await pubUpdater.isUpToDate(
-      packageName: 'myPackage', currentVersion: '0.1.0');
+    packageName: 'my_package',
+    currentVersion: '0.1.0',
+  );
 
   if (!isUpToDate) {
     // Upgrade to the latest version if not up to date.
-    await pubUpdater.update(packageName: 'myPackage');
+    await pubUpdater.update(packageName: 'my_package');
   }
 }
