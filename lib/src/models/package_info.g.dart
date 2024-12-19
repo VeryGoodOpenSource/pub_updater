99fccbb33a02128732617ce2abdd7c7f7b55d717
Tom Arra <tarra3@gmail.com>
2024-12-19 14:04:00 -0600

    update generated file

--- lib/src/models/package_info.g.dart
+++ lib/src/models/package_info.g.dart
@@ -1,6 +1,6 @@
 // GENERATED CODE - DO NOT MODIFY BY HAND
 
-// ignore_for_file: implicit_dynamic_parameter, cast_nullable_to_non_nullable, require_trailing_commas, lines_longer_than_80_chars
+// ignore_for_file: document_ignores, implicit_dynamic_parameter, cast_nullable_to_non_nullable, require_trailing_commas, lines_longer_than_80_chars
 
 part of 'package_info.dart';
 
